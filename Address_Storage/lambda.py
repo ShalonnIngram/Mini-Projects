c2ef793edaa511182f53031bfad0462d9652c3b6
ShalonnIngram <mscianika@gmail.com>
2024-05-02 09:40:08 -0400

    Update lambda.py

--- Address_Storage/lambda.py
+++ Address_Storage/lambda.py
@@ -1,1 +1,8 @@
+import boto3
+import json 
 
+glue_client = boto3.client('glue')
+
+def lambda_handler(event, context):
+    glue_client.start_job_run(JobName = 'marketingetlmay')
+    return 'Your Glue Job has started'
